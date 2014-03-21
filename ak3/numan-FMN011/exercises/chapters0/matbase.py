0fadc34dd4de3ff5ba1661131a135ee005899aba
ragadeeshu <ragnar_mellbin@hotmail.com>
2014-03-21 21:24:17 +0100

    Fixed stupid error

--- ak3/numan-FMN011/exercises/chapters0/matbase.py
+++ ak3/numan-FMN011/exercises/chapters0/matbase.py
@@ -1,45 +1,10 @@
 #!/usr/bin/env python
 
-"""horner.py: Horner's method 0.1 C2"""
+"""matbase.py: Basic imports for SCIENCE"""
 
 __author__      = "Ragnar Mellbin"
 
-from matbase import *
-
-def horner(polynom, x) -> float:
-	b=0
-	for a in np.nditer(polynom[::-1]):
-		b=a+b*x
-	return b
-
-polynom = np.ones(100)
-polynom [1::2]=-1
-x=1.00001
-
-#for a in np.nditer(polynom):
-#	print(a)
-
-#Basic Horner
-print("Basic Horner gives: ")
-eval = horner(polynom,x)
-print(eval)
-print()
-
-#Seperated Horner
-negpolynom = np.copy(polynom)
-polynom[1::2]=0
-negpolynom[::2]=0
-
-#for a in np.nditer(negpolynom):
-#	print(a)
-#for a in np.nditer(polynom):
-#	print(a)
-
-sum = horner(polynom,x)+horner(negpolynom,x)
-print("Seperated Horner gives: ")
-print(sum)
-print()
-
-print("Diff: ")
-print(sum-eval)
-
+import numpy as np
+import scipy as sp
+import matplotlib as mpl
+import matplotlib.pyplot as plt
